b9b6df642b773239bfde3853ea02e4f22aca9e11
Lucas Chipolari <zlucaschipolariz@gmail.com>
2024-04-09 07:13:13 -0300

    Cree carpetas y termine el ejercicio 1

---
+++ Ejercicio 1/index.js
@@ -0,0 +1,8 @@
+// 1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+document.write('<h1>Lista de Meses</h1>');
+for (const mes of meses) {
+    document.write('<li>'+mes+'</li>')
+}
