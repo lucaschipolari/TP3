6e0aa276de51d9841ad664346e4b1d6bf05f5c69
lucas <zlucaschipolariz@gmail.com>
2024-04-13 18:17:08 -0300

    Corregí el ejercicio 1

--- Ejercicio 1/index.js
+++ Ejercicio 1/index.js
@@ -3,6 +3,8 @@
 const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 document.write('<h1>Lista de Meses</h1>');
+document.write('<ul>');
 for (const mes of meses) {
     document.write('<li>'+mes+'</li>')
 }
+document.write('</ul>');
